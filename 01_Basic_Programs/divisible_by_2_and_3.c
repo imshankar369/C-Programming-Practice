50e5678a1e3557c6f40d82d472fe6a79daeac86c
imshankar369 <imshankar369@gmail.com>
2026-07-24 03:34:09 +0600

    Add Divisible by 2 and 3 program

---
+++ 01_Basic_Programs/divisible_by_2_and_3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if(num % 2 == 0 && num % 3 == 0)
+        printf("The number is divisible by both 2 and 3.\n");
+    else
+        printf("The number is not divisible by both 2 and 3.\n");
+
+    return 0;
+}
