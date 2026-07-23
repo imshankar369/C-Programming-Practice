1288ca4a4415c5a43def8b014fe454e4e77085bb
imshankar369 <imshankar369@gmail.com>
2026-07-24 03:49:45 +0600

    Add Perimeter of Rectangle program

---
+++ 01_Basic_Programs/perimeter_of_rectangle.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    float length, width, perimeter;
+
+    printf("Enter length: ");
+    scanf("%f", &length);
+
+    printf("Enter width: ");
+    scanf("%f", &width);
+
+    perimeter = 2 * (length + width);
+
+    printf("Perimeter = %.2f\n", perimeter);
+
+    return 0;
+}
