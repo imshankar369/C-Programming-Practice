4f083e63749a9b0044a6f2b32ca18c507966b931
imshankar369 <imshankar369@gmail.com>
2026-07-24 03:07:07 +0600

    Add program to Check Character Case

---
+++ 01_Basic_Programs/uppercase_lowercase.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    char ch;
+
+    printf("Enter a character: ");
+    scanf(" %c", &ch);
+
+    if(ch >= 'A' && ch <= 'Z')
+        printf("Uppercase Letter\n");
+
+    else if(ch >= 'a' && ch <= 'z')
+        printf("Lowercase Letter\n");
+
+    else
+        printf("Not an Alphabet\n");
+
+    return 0;
+}
