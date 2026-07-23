b3b8d4b1f9f154f66bf7b2bba57b04f89bd46cc0
imshankar369 <imshankar369@gmail.com>
2026-07-24 04:15:27 +0600

    Add Kilogram to Gram Converter

---
+++ 01_Basic_Programs/kilogram_to_gram.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    float kilogram;
+
+    printf("Enter kilogram: ");
+    scanf("%f", &kilogram);
+
+    printf("Gram = %.2f\n", kilogram * 1000);
+
+    return 0;
+}e
