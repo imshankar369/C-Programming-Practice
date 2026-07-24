0ecddd32eaa50a1fa35cb27485bc664e75d0c44a
imshankar369 <imshankar369@gmail.com>
2026-07-24 18:18:53 +0600

    Add program to count odd digits in a number
    This program counts the number of odd digits in a given integer.

---
+++ Loop/count_odd_digits.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int n, digit, count = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    while (n != 0) {
+        digit = n % 10;
+
+        if (digit % 2 != 0)
+            count++;
+
+        n /= 10;
+    }
+
+    printf("Odd Digits = %d", count);
+
+    return 0;
+}
