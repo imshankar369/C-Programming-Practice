21acea21d33dc45731d3d5d8639f8aa8b001e494
imshankar369 <imshankar369@gmail.com>
2026-07-24 18:16:16 +0600

    Add program to find the first digit of a number

---
+++ Loop/first_digit.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    while (n >= 10) {
+        n /= 10;
+    }
+
+    printf("First Digit = %d", n);
+
+    return 0;
+}
