fca9865a3499b6f53ca9cf18233666485a25601a
imshankar369 <imshankar369@gmail.com>
2026-07-24 18:19:43 +0600

    Add program to sum odd digits of a number

---
+++ Loop/sum_of_odd_digits.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int n, digit, sum = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    while (n != 0) {
+        digit = n % 10;
+
+        if (digit % 2 != 0)
+            sum += digit;
+
+        n /= 10;
+    }
+
+    printf("Sum of Odd Digits = %d", sum);
+
+    return 0;
+}
