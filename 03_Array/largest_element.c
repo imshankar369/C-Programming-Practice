596fc5f2071bc76e4f2d2a9429737efa0f808f12
Shiv Shankar <imshankar369@gmail.com>
2026-07-25 17:30:40 +0600

    Add program to find the largest element in an array

---
+++ 03_Array/largest_element.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, largest;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    largest = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > largest)
+            largest = arr[i];
+    }
+
+    printf("Largest Element = %d\n", largest);
+
+    return 0;
+}
