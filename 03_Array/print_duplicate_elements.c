2e2cf98f3a2bf6ae48ee2d2a93a2876b1690a8c8
Shiv Shankar <imshankar369@gmail.com>
2026-07-25 17:36:11 +0600

    Add program to print duplicate elements in array

---
+++ 03_Array/print_duplicate_elements.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100], n;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("Duplicate Elements: ");
+
+    for(int i = 0; i < n; i++) {
+        for(int j = i + 1; j < n; j++) {
+            if(arr[i] == arr[j]) {
+                printf("%d ", arr[i]);
+                break;
+            }
+        }
+    }
+
+    return 0;
+}
