19b36895548593c076d2f8130c285178eff11ae1
Shiv Shankar <imshankar369@gmail.com>
2026-07-26 18:21:42 +0600

    Add program to find first odd element in array
    This program prompts the user to enter an array of integers and finds the first odd element in the array.

---
+++ 03_Array/find_first_odd_element.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100], n;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter array elements:\n");
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] % 2 != 0) {
+            printf("First Odd Element = %d", arr[i]);
+            return 0;
+        }
+    }
+
+    printf("No odd element found.");
+
+    return 0;
+}
