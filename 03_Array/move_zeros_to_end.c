6e9ccbf6d720659cd22a96f85bd17af9b84fa552
Shiv Shankar <imshankar369@gmail.com>
2026-07-26 18:10:43 +0600

    Add program to move zeros to the end of an array
    This program takes an array of integers and moves all zeros to the end while maintaining the order of non-zero elements.

---
+++ 03_Array/move_zeros_to_end.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, j = 0, temp;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter array elements:\n");
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] != 0) {
+            temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            j++;
+        }
+    }
+
+    printf("Array after moving zeros to end:\n");
+
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
