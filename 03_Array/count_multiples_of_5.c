e5c2e2f57ba0e3645024adec20339d1a874181fd
Shiv Shankar <imshankar369@gmail.com>
2026-07-26 18:19:54 +0600

    Create count_multiples_of_5.c

---
+++ 03_Array/count_multiples_of_5.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, count = 0;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter array elements:\n");
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] % 5 == 0)
+            count++;
+    }
+
+    printf("Multiples of 5 = %d", count);
+
+    return 0;
+}
