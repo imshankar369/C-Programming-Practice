924013edd046ccc03cae4667a6cb2439c5c823b4
Shiv Shankar <imshankar369@gmail.com>
2026-07-27 18:59:31 +0600

    Add string length calculation without using strlen

---
+++ 04_String/string_length_without_strlen.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int length = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    while (str[length] != '\0')
+        length++;
+
+    if (str[length - 1] == '\n')
+        length--;
+
+    printf("Length = %d", length);
+
+    return 0;
+}
