8cb2b2d2496ced957e9bf2aab69e27406cad3d38
Shiv Shankar <imshankar369@gmail.com>
2026-07-27 19:00:29 +0600

    Add string concatenation without using strcat

---
+++ 04_String/concatenate_strings_without_strcat.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    char str1[200], str2[100];
+    int i = 0, j = 0;
+
+    printf("Enter first string: ");
+    fgets(str1, sizeof(str1), stdin);
+
+    printf("Enter second string: ");
+    fgets(str2, sizeof(str2), stdin);
+
+    while (str1[i] != '\0')
+        i++;
+
+    if (str1[i - 1] == '\n')
+        i--;
+
+    while (str2[j] != '\0') {
+        str1[i] = str2[j];
+        i++;
+        j++;
+    }
+
+    str1[i] = '\0';
+
+    printf("Concatenated String: %s", str1);
+
+    return 0;
+}
