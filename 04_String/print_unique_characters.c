5655fa055cdd5f1ee8a94e0018c3e2f09d82d35e
Shiv Shankar <imshankar369@gmail.com>
2026-07-28 21:37:39 +0600

    Add program to print unique characters from a string
    This program takes a string input and prints the unique characters found in it.

---
+++ 04_String/print_unique_characters.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    printf("Unique characters: ");
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        int count = 0;
+
+        for (int j = 0; str[j] != '\0'; j++) {
+            if (str[i] == str[j])
+                count++;
+        }
+
+        if (count == 1)
+            printf("%c ", str[i]);
+    }
+
+    return 0;
+}
