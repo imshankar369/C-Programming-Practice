a00c1b0a8e97d92ccecbf0bdba41cae699432689
Shiv Shankar <imshankar369@gmail.com>
2026-07-28 21:37:03 +0600

    Add program to remove all consonants from a string

---
+++ 04_String/remove_all_consonants.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i, j = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    for (i = 0; str[i] != '\0'; i++) {
+        if ((str[i] == 'a' || str[i] == 'e' || str[i] == 'i' ||
+             str[i] == 'o' || str[i] == 'u' ||
+             str[i] == 'A' || str[i] == 'E' || str[i] == 'I' ||
+             str[i] == 'O' || str[i] == 'U') ||
+            !(('A' <= str[i] && str[i] <= 'Z') ||
+              ('a' <= str[i] && str[i] <= 'z'))) {
+            str[j++] = str[i];
+        }
+    }
+
+    str[j] = '\0';
+
+    printf("After removing consonants: %s", str);
+
+    return 0;
+}
