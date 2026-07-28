975c3c531f61b6fb01dd5156cc71a8339520df91
Shiv Shankar <imshankar369@gmail.com>
2026-07-28 21:34:16 +0600

    Add anagram checking program in C

---
+++ 04_String/check_anagram.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str1[100], str2[100];
+    int count1[256] = {0}, count2[256] = {0};
+
+    printf("Enter first string: ");
+    fgets(str1, sizeof(str1), stdin);
+
+    printf("Enter second string: ");
+    fgets(str2, sizeof(str2), stdin);
+
+    str1[strcspn(str1, "\n")] = '\0';
+    str2[strcspn(str2, "\n")] = '\0';
+
+    for (int i = 0; str1[i] != '\0'; i++)
+        count1[(unsigned char)str1[i]]++;
+
+    for (int i = 0; str2[i] != '\0'; i++)
+        count2[(unsigned char)str2[i]]++;
+
+    for (int i = 0; i < 256; i++) {
+        if (count1[i] != count2[i]) {
+            printf("Not Anagrams");
+            return 0;
+        }
+    }
+
+    printf("Anagrams");
+
+    return 0;
+}
