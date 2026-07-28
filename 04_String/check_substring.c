0ebea48543343fcf9e51ba42ed203de3a3cb1497
Shiv Shankar <imshankar369@gmail.com>
2026-07-28 21:47:25 +0600

    Add substring search functionality in C

---
+++ 04_String/check_substring.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[200], sub[100];
+
+    printf("Enter main string: ");
+    fgets(str, sizeof(str), stdin);
+
+    printf("Enter substring: ");
+    fgets(sub, sizeof(sub), stdin);
+
+    str[strcspn(str, "\n")] = '\0';
+    sub[strcspn(sub, "\n")] = '\0';
+
+    if (strstr(str, sub) != NULL)
+        printf("Substring found");
+    else
+        printf("Substring not found");
+
+    return 0;
+}
