b8c58751eecbe9d1a6d27a63109efb5f01d5cc0d
Shiv Shankar <imshankar369@gmail.com>
2026-07-29 23:51:42 +0600

    Implement string concatenation function myStrcat

---
+++ 04_String/implement_strcat.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void myStrcat(char str1[], char str2[]) {
+    int i = 0, j = 0;
+
+    while (str1[i] != '\0')
+        i++;
+
+    if (str1[i - 1] == '\n')
+        i--;
+
+    while (str2[j] != '\0') {
+        str1[i] = str2[j];
+        i++;
+        j++;
+    }
+
+    str1[i] = '\0';
+}
+
+int main() {
+    char str1[200], str2[100];
+
+    printf("Enter first string: ");
+    fgets(str1, sizeof(str1), stdin);
+
+    printf("Enter second string: ");
+    fgets(str2, sizeof(str2), stdin);
+
+    myStrcat(str1, str2);
+
+    printf("Concatenated String: %s", str1);
+
+    return 0;
+}
