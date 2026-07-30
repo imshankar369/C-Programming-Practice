082959dea822b35041569a37734a9c7599e717ef
Shiv Shankar <imshankar369@gmail.com>
2026-07-30 22:45:06 +0600

    Implement array reversal using pointers
    This program reverses an array using pointers. It takes user input for the array size and elements, then swaps elements from the start and end until the array is reversed.

---
+++ 05_Pointer/reverse_array_using_pointer.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+
+    int arr[100], n;
+    int *start, *end, temp;
+
+    printf("Enter size: ");
+    scanf("%d", &n);
+
+    printf("Enter elements:\n");
+
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    start = arr;
+    end = arr + n - 1;
+
+    while(start < end) {
+
+        temp = *start;
+        *start = *end;
+        *end = temp;
+
+        start++;
+        end--;
+    }
+
+    printf("Reversed array:\n");
+
+    for(int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
