f58586173955d9b2983ec089f13f8b98f0d7e3ed
Shiv Shankar <imshankar369@gmail.com>
2026-07-30 22:42:50 +0600

    Implement pointer modification example in C

---
+++ 05_Pointer/pointer_modify_value.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+    int num = 10;
+    int *ptr = &num;
+
+    printf("Before modification: %d\n", num);
+
+    *ptr = 50;
+
+    printf("After modification: %d\n", num);
+
+    return 0;
+}
