903e330e7d3efdb2e94a2778e35ab03384458276
Shiv Shankar <imshankar369@gmail.com>
2026-07-30 23:56:24 +0600

    Add basic function pointer example in C

---
+++ 05_Pointer/function_pointer_basic.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int main() {
+
+    int (*ptr)(int, int);
+
+    ptr = add;
+
+    printf("Sum = %d", ptr(10, 20));
+
+    return 0;
+}
