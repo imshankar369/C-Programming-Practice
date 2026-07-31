a89c115489ff77ef0a31c853fc1dec0f2a4e98ff
Shiv Shankar <imshankar369@gmail.com>
2026-07-31 20:48:06 +0600

    Add pointer to array example in pointer_to_array.c
    Implement pointer to array example in C.

---
+++ 05_Pointer/pointer_to_array.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+    int arr[5] = {10, 20, 30, 40, 50};
+
+    int (*ptr)[5];
+
+    ptr = &arr;
+
+    printf("Array elements:\n");
+
+    for(int i = 0; i < 5; i++)
+        printf("%d ", (*ptr)[i]);
+
+    return 0;
+}
