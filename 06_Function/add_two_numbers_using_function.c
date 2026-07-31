7d1837921676265eabb58fded3007f9f89ec3fbb
Shiv Shankar <imshankar369@gmail.com>
2026-07-31 20:56:53 +0600

    Add function to sum two numbers in C

---
+++ 06_Function/add_two_numbers_using_function.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int main() {
+
+    int a, b;
+
+    printf("Enter two numbers: ");
+    scanf("%d%d", &a, &b);
+
+    printf("Sum = %d", add(a, b));
+
+    return 0;
+}
