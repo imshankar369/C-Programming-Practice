fb7efeae6a50fa16f49b8d5b9b5ae2c9e7362a2d
Shiv Shankar <imshankar369@gmail.com>
2026-08-01 21:28:38 +0600

    Add swap function using call by reference

---
+++ 06_Function/swap_using_call_by_reference.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+
+    int a, b;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    swap(&a, &b);
+
+    printf("After swap: a = %d, b = %d", a, b);
+
+    return 0;
+}
