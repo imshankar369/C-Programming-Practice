efc1df3140589b6e04503efb4e1ab9f12760f9ec
Shiv Shankar <imshankar369@gmail.com>
2026-08-02 22:10:49 +0600

    Add recursive power function in C

---
+++ 06_Function/recursive_power.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+long long power(int base, int exponent) {
+
+    if (exponent == 0)
+        return 1;
+
+    return base * power(base, exponent - 1);
+}
+
+int main() {
+
+    int base, exponent;
+
+    printf("Enter base and exponent: ");
+    scanf("%d %d", &base, &exponent);
+
+    printf("Result = %lld", power(base, exponent));
+
+    return 0;
+}
