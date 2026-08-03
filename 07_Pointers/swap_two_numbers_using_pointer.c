a1c45b885e4a7f844eda21a720ba21dee14ea298
Shiv Shankar <imshankar369@gmail.com>
2026-08-03 21:39:07 +0600

    Create swap_two_numbers_using_pointer.c

---
+++ 07_Pointers/swap_two_numbers_using_pointer.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main()
+{
+    int a, b;
+
+    printf("Enter two numbers: ");
+    scanf("%d%d", &a, &b);
+
+    swap(&a, &b);
+
+    printf("After Swap: %d %d\n", a, b);
+
+    return 0;
+}
