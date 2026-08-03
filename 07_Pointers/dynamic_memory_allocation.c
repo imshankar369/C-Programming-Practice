922a6aef00d51708b22d2b5fa60f77a1537eaf22
Shiv Shankar <imshankar369@gmail.com>
2026-08-03 21:42:29 +0600

    Create dynamic_memory_allocation.c

---
+++ 07_Pointers/dynamic_memory_allocation.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int *arr = (int *)malloc(n * sizeof(int));
+
+    if(arr == NULL)
+    {
+        printf("Memory Allocation Failed!\n");
+        return 1;
+    }
+
+    printf("Enter elements:\n");
+
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("Elements are:\n");
+
+    for(int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    free(arr);
+
+    return 0;
+}
