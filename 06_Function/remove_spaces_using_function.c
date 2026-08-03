026996950dc20ccb551d670306635563457eb8e9
Shiv Shankar <imshankar369@gmail.com>
2026-08-03 21:20:45 +0600

    Refactor removeSpaces function and update main
    Refactored removeSpaces function to eliminate spaces from the input string and updated main function to call it.

---
+++ 06_Function/remove_spaces_using_function.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void removeSpaces(char str[])
+{
+    int i, j = 0;
+
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] != ' ')
+        {
+            str[j++] = str[i];
+        }
+    }
+
+    str[j] = '\0';
+}
+
+int main()
+{
+    char str[100];
+
+    printf("Enter string: ");
+    fgets(str, sizeof(str), stdin);
+
+    removeSpaces(str);
+
+    printf("Result = %s", str);
+
+    return 0;
+}
