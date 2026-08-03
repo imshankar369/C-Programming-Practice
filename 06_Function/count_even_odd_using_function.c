a93d78b48516f2c9e9e600237651201d7c4d5238
Shiv Shankar <imshankar369@gmail.com>
2026-08-03 21:18:38 +0600

    Add function to count even and odd numbers

---
+++ 06_Function/count_even_odd_using_function.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void countEvenOdd(int arr[], int n)
+{
+    int even = 0, odd = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] % 2 == 0)
+            even++;
+        else
+            odd++;
+    }
+
+    printf("Even = %d\n", even);
+    printf("Odd = %d\n", odd);
+}
+
+int main()
+{
+    int n;
+
+    printf("Enter size: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter elements:\n");
+
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    countEvenOdd(arr, n);
+
+    return 0;
+}
